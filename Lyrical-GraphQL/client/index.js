eb2d482fab8ebde0d34015c4984d3d9a896a7636
Vahid Çelik <vahidcelik@gmail.com>
2020-04-14 04:41:09 +0300

    Create Songlist component

--- Lyrical-GraphQL/client/index.js
+++ Lyrical-GraphQL/client/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+import SongList from './components/SongList';
+
 
 const client = new ApolloClient({});
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Waayd</div>
+      <SongList />
     </ApolloProvider>
   )
 };
